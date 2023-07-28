8513971cc3ebcf4a0671ff670ec1761220181495
deanchen <antlet2004@gmail.com>
2023-07-28 17:22:01 +0800

    chore: first

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,105 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// mui theme
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3FDEAE',
+    },
+    secondary: {
+      main: '#3f51b5',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Product Sans',
+    ].join(','),
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      letterSpacing: '-0.01562em',
+    },
+    h2: {
+      fontSize: '2.5rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      letterSpacing: '-0.00833em',
+    },
+    h3: {
+      fontSize: '2rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      letterSpacing: '0em',
+    },
+    h4: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      letterSpacing: '0.00735em',
+    },
+    h5: {
+      fontSize: '1.2rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      letterSpacing: '0em',
+    },
+    h6: {
+      fontSize: '1rem',
+      fontWeight: 500,
+      lineHeight: 1.2,
+      letterSpacing: '0.0075em',
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.75,
+      letterSpacing: '0.00938em',
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      lineHeight: 1.57,
+      letterSpacing: '0.00714em',
+    },
+    body1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.5,
+      letterSpacing: '0.00938em',
+    },
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+      lineHeight: 1.43,
+      letterSpacing: '0.01071em',
+    },
+    button: {
+      fontSize: '0.875rem', 
+      fontWeight: 500,
+      lineHeight: 1.75,
+      letterSpacing: '0.02857em',
+      textTransform: 'uppercase',
+      },
+    caption: {
+      fontSize: '0.75rem',
+      fontWeight: 400,
+      lineHeight: 1.66,
+      letterSpacing: '0.03333em',
+    },
+  },
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
