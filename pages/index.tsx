1b43cc7f2ad2d096da03760abc7ceaed99b45415
root <root@devmachine-0.devmachine.dev.svc.cluster.local>
2023-07-29 02:18:08 +0000

    chore: home layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Paper, Typography } from '@mui/material';
+import { Paper, Box, Typography, Avatar } from '@mui/material';
+import UserIcon from '@mui/icons-material/Person';
 
 const Home: NextPage = () => {
   return (
@@ -15,10 +16,33 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Typography variant="h5" >
-          Sign In 
-        </Typography>
-        
+        <Box 
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            height: '100vh',
+            width: '100vw',
+            }}
+        >
+          <Typography variant="h5" 
+            sx={{
+              marginTop: '3rem',
+              }}
+          >
+            Sign In 
+          </Typography>
+          <Avatar
+            sx={{
+              width: 104,
+              height: 104,
+              marginTop: '1.7rem',
+              }}
+          >
+            <UserIcon />
+          </Avatar>
+        </Box>
       </main>
     </div>
   )
