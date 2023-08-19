27d7378ea465367d4674dc06818fdcf850180e78
dadiorchen <antlet2004@gmail.com>
2023-08-19 11:47:46 +0800

    chore: good demo with wallet on map

--- pages/wallets/[walletid].tsx
+++ pages/wallets/[walletid].tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { Wallet } from '../../models/entities/Wallet';
 import formatWalletName from '../../models/wallet/formatWalletName';
+import shortenUuid from '../../models/common/shortenUuid';
 import { Token } from '../../models/entities/Token';
 import useTransferWizard from '../../models/transfer/useTransferWizard';
 
@@ -23,18 +24,12 @@ export default function Wallet() {
       } else {
         const wallet = {
           id: faker.datatype.uuid(),
-          name: faker.internet.userName(),
+          name: 'wallet2',
           balance: faker.datatype.number({ min: 1000, max: 100000 }),
           createdAt: faker.date.past(),
           logo: faker.image.url(),
         };
         setCurrentWallet(wallet);
-        const token = {
-          id: faker.datatype.uuid(),
-          walletId: wallet.id,
-          createdAt: faker.date.past(),
-        };
-        setChosenToken(token);
       }
     }
 
@@ -51,6 +46,8 @@ export default function Wallet() {
     });
   }, [currentWallet, chosenToken]);
 
+  const title = (chosenToken && ('Token #' + shortenUuid(chosenToken.id))) || (currentWallet && formatWalletName(currentWallet)) || '';
+
   return (
     <Box
       sx={{
@@ -61,7 +58,7 @@ export default function Wallet() {
       }}
     >
       <Header
-        title={(currentWallet && formatWalletName(currentWallet)) || ''}
+        title={title}
         backLink="/home"
         forwardLink={transferWizard.isTransferable ? '/transfer/step1' : ''}
         forwardText={transferWizard.isTransferable ? 'Transfer' : ''}
@@ -73,10 +70,30 @@ export default function Wallet() {
         }}
       >
         <iframe
-          src="https://alpha-dev.treetracker.org/top"
+          //src="https://alpha-dev.treetracker.org/trees/1000249?bounds=-77.3876738548279,40.367375653694786,-77.37879037857057,40.38084568185104"
+          src="https://alpha-dev.treetracker.org/wallets/wallet2?bounds=-74.01040792465211,40.712768515280914,-74.00152444839479,40.712768515280914"
           height="100%"
           width="100%"
           style={{ border: 'none' }}
+          ref={(iframe) => {
+            console.log('xxx iframe: ', iframe);
+            // listen for messages from the iframe
+            window.addEventListener(
+              'message',
+              (event) => {
+                console.log('xxx mother page get event: ', event);
+                if(event.data.id){
+                  const token = {
+                    id: faker.datatype.uuid(),
+                    walletId: currentWallet?.id || 'no',
+                    createdAt: faker.date.past(),
+                  };
+                  setChosenToken(token);
+                }
+              },
+              false
+            );
+          }}
         />
       </Box>
     </Box>
