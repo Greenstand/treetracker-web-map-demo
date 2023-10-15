962d5a9d0e816f4c3025925b39b81d29ab6e249f
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-10-15 13:31:10 +0800

    added moving other content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,196 +21,195 @@ import SettingsIcon from '../images/Settings.svg';
 const Navbar = ({ user, open, toggleDrawer }) => {
   return (
     <>
-      <Grid
-        sx={{
-          width: '100vw',
+      <div
+        style={{
           backgroundColor: '#fff',
+          width: '100vw',
           height: '10vh',
-          elevation: 30,
-          zIndex: 1201,
+          zIndex: '1201',
           position: 'fixed',
           top: open ? '0' : '-10vh',
-          transition: 'all 0.25s',
-        }}
-        container
-        rowSpacing={3}
-        style={{
+          left: '0',
           display: 'flex',
-          alignItems: 'flex-end',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          paddingLeft: '16px',
+          paddingRight: '16px',
+          transition: 'all 0.5s',
         }}
       >
-        <Grid
-          item
-          xs={2}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
+        <IconButton onClick={toggleDrawer} size="medium">
+          <CloseRoundedIcon style={{ color: '#000', fontSize: '28px' }} />
+        </IconButton>
+        <Typography
+          sx={{ width: '100%', textAlign: 'center', fontSize: '20px' }}
+          variant="h4"
         >
-          <IconButton onClick={toggleDrawer} size="large">
-            <CloseRoundedIcon fontSize="large" sx={{ color: '#000' }} />
-          </IconButton>
-        </Grid>
-        <Grid
-          item
-          xs={8}
-          sx={{ textAlign: 'center' }}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+          Profile
+        </Typography>
+        <IconButton size="medium">
+          <LogoutRoundedIcon sx={{ color: '#000', fontSize: '28px' }} />
+        </IconButton>
+      </div>
+      <div
+        style={{
+          position: 'fixed',
+          left: open ? '0' : 'calc(-70vw - 5px)',
+          top: '0',
+          zIndex: '3',
+          height: '100vh',
+          width: '70vw',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          paddingTop: '10vh',
+          paddingLeft: '24px',
+          transition: 'all 0.5s',
+        }}
+      >
+        <Avatar
+          src={user.avatar}
+          sx={{
+            width: 69,
+            height: 69,
           }}
-        >
-          <Typography
-            sx={{ width: '100%', height: '5vh', textAlign: 'center' }}
-            variant="h4"
-          >
-            Profile
-          </Typography>
-        </Grid>
-        <Grid xs={2} sx={{ paddingBottom: 1 }}>
-          <IconButton size="large">
-            <LogoutRoundedIcon fontSize="large" sx={{ color: '#000' }} />
-          </IconButton>
-        </Grid>
-      </Grid>
-      <Drawer variant="temporary" open={open} onClose={toggleDrawer}>
-        <Box sx={{ width: '70vw', marginTop: '10vh', paddingLeft: 7 }}>
-          <Avatar
-            src={user.avatar}
-            sx={{
-              width: 69,
-              height: 69,
-            }}
-          />
-          <Typography variant="h4" sx={{ marginTop: 3 }}>
-            {user.firstName + ' ' + user.lastName}
-          </Typography>
-          <Typography variant="subtitle2">
-            {user.firstName.toLowerCase() + '.' + user.lastName.toLowerCase()}
-            @gmail.com
-          </Typography>
-          <List sx={{ marginTop: 8, marginLeft: 0, paddingLeft: 0 }}>
-            <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
-              <Stack
-                direction="row"
-                sx={{ marginTop: 1 }}
+        />
+        <Typography variant="h4" sx={{ marginTop: 3 }}>
+          {user.firstName + ' ' + user.lastName}
+        </Typography>
+        <Typography variant="subtitle2" sx={{ fontSize: '13px' }}>
+          {user.firstName.toLowerCase() + '.' + user.lastName.toLowerCase()}
+          @gmail.com
+        </Typography>
+        <List sx={{ marginTop: 4, marginLeft: 0, paddingLeft: 0 }}>
+          <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
+            <Stack
+              direction="row"
+              sx={{ marginTop: 1 }}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  width: 43,
+                  height: 43,
+                  backgroundColor: '#F1F5FF',
+                  borderRadius: '50%',
+                }}
                 style={{
                   display: 'flex',
                   alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
-                <Box
-                  sx={{
-                    width: 43,
-                    height: 43,
-                    backgroundColor: '#F1F5FF',
-                    borderRadius: '50%',
-                  }}
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <HistoryRoundedIcon />
-                </Box>
-                <Typography
-                  variant="h6"
-                  sx={{ paddingLeft: 6, fontSize: '1rem' }}
-                >
-                  Transaction History
-                </Typography>
-              </Stack>
-            </ListItem>
-            <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
-              <Stack
-                direction="row"
+                <HistoryRoundedIcon style={{ fontSize: '20px' }} />
+              </Box>
+              <Typography
+                variant="h6"
+                sx={{ paddingLeft: 4, fontSize: '16px' }}
+              >
+                Transaction History
+              </Typography>
+            </Stack>
+          </ListItem>
+          <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
+            <Stack
+              direction="row"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  width: 43,
+                  height: 43,
+                  backgroundColor: '#F1F5FF',
+                  borderRadius: '50%',
+                }}
                 style={{
                   display: 'flex',
                   alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
-                <Box
-                  sx={{
-                    width: 43,
-                    height: 43,
-                    backgroundColor: '#F1F5FF',
-                    borderRadius: '50%',
-                  }}
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <WalletRoundedIcon />
-                </Box>
-                <Typography variant="h6" sx={{ paddingLeft: 6 }}>
-                  All Wallets
-                </Typography>
-              </Stack>
-            </ListItem>
-            <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
-              <Stack
-                direction="row"
+                <WalletRoundedIcon style={{ fontSize: '20px' }} />
+              </Box>
+              <Typography
+                variant="h6"
+                sx={{ paddingLeft: 4, fontSize: '16px' }}
+              >
+                All Wallets
+              </Typography>
+            </Stack>
+          </ListItem>
+          <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
+            <Stack
+              direction="row"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  width: 43,
+                  height: 43,
+                  backgroundColor: '#F1F5FF',
+                  borderRadius: '50%',
+                }}
                 style={{
                   display: 'flex',
                   alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
-                <Box
-                  sx={{
-                    width: 43,
-                    height: 43,
-                    backgroundColor: '#F1F5FF',
-                    borderRadius: '50%',
-                  }}
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <PasswordRoundedIcon />
-                </Box>
-                <Typography variant="h6" sx={{ paddingLeft: 6 }}>
-                  Change Password
-                </Typography>
-              </Stack>
-            </ListItem>
-            <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
-              <Stack
-                direction="row"
+                <PasswordRoundedIcon style={{ fontSize: '20px' }} />
+              </Box>
+              <Typography
+                variant="h6"
+                sx={{ paddingLeft: 4, fontSize: '16px' }}
+              >
+                Change Password
+              </Typography>
+            </Stack>
+          </ListItem>
+          <ListItem sx={{ marginLeft: 0, paddingLeft: 0 }}>
+            <Stack
+              direction="row"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  width: 43,
+                  height: 43,
+                  backgroundColor: '#F1F5FF',
+                  borderRadius: '50%',
+                }}
                 style={{
                   display: 'flex',
                   alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
-                <Box
-                  sx={{
-                    width: 43,
-                    height: 43,
-                    backgroundColor: '#F1F5FF',
-                    borderRadius: '50%',
-                  }}
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <SettingsIcon />
-                </Box>
-                <Typography variant="h6" sx={{ paddingLeft: 6 }}>
-                  Settings
-                </Typography>
-              </Stack>
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
+                <SettingsIcon style={{ fontSize: '20px' }} />
+              </Box>
+              <Typography
+                variant="h6"
+                sx={{ paddingLeft: 4, fontSize: '16px' }}
+              >
+                Settings
+              </Typography>
+            </Stack>
+          </ListItem>
+        </List>
+      </div>
     </>
   );
 };
